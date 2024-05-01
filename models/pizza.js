593f51a50dd2bcdc792458a9430b5db968ac04a2
Jayden Taylor <j.thomastt1@gmail.com>
2024-05-01 11:51:28 -0700

    Set up MongoDB/ CRUD routes / connected APIs to front-end form

---
+++ models/pizza.js
@@ -0,0 +1,25 @@
+// create the Pizza model using the PizzaSchema
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: Array
+});
+
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the pizza model
+module.exports = Pizza;
